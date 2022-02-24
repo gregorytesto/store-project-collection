0b7cc2e17741df7bdc5d941ba13d189874fb040a
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-02-24 16:14:04 -0500

    created cards (need more styling), header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import Card from './Card';
 
 function App() {
+  const [ projects, setProjects ] = useState([]) 
+
+
+  
+  const allCards = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>8.2 eCommerce Project Collection</h1>
       </header>
+
+      <div className="cards-container">
+        {allCards}
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
     </div>
   );
 }
